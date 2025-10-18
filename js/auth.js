37cd46db9774de115c2a65126d4c0c83c6f61f2c
mehdi00712 <mbhoyroo246@gmail.com>
2025-10-18 08:57:26 +0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { app } from "./firebase-config.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyC2jf5v9p8kVXP3weiPvHm8piHDbP6XaRw",
-  authDomain: "mautours-60318.firebaseapp.com",
-  projectId: "mautours-60318",
-  storageBucket: "mautours-60318.firebasestorage.app",
-  messagingSenderId: "1009871171111",
-  appId: "1:1009871171111:web:d4369386c1a958aa18a802",
-  measurementId: "G-882H9VPLJT",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// DOM elements
 const emailInput = document.getElementById("authEmail");
 const passwordInput = document.getElementById("authPassword");
 const loginBtn = document.getElementById("loginBtn");
@@ -24,10 +14,8 @@ const signupBtn = document.getElementById("signupBtn");
 
 loginBtn.addEventListener("click", async (e) => {
   e.preventDefault();
-  const email = emailInput.value;
-  const password = passwordInput.value;
   try {
-    await signInWithEmailAndPassword(auth, email, password);
+    await signInWithEmailAndPassword(auth, emailInput.value, passwordInput.value);
     alert("Login successful ✅");
     window.location.href = "admin.html";
   } catch (err) {
@@ -36,10 +24,8 @@ loginBtn.addEventListener("click", async (e) => {
 });
 
 signupBtn.addEventListener("click", async () => {
-  const email = emailInput.value;
-  const password = passwordInput.value;
   try {
-    await createUserWithEmailAndPassword(auth, email, password);
+    await createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value);
     alert("Account created successfully ✅");
   } catch (err) {
     alert("Signup failed: " + err.message);
