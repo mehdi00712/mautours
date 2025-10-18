b252d85f38a864800abcd1e816b29cb8ca49b6bb
mehdi00712 <mbhoyroo246@gmail.com>
2025-10-18 19:41:59 +0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,17 @@
-// js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 
-// ✅ Export only the configuration object.
-// Do NOT initialize Firebase here; the other scripts will import it.
-
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyC2jf5v9p8kVXP3weiPvHm8piHDbP6XaRw",
   authDomain: "mautours-60318.firebaseapp.com",
   projectId: "mautours-60318",
-  storageBucket: "mautours-60318.appspot.com",
+  storageBucket: "mautours-60318.firebasestorage.app",
   messagingSenderId: "1009871171111",
   appId: "1:1009871171111:web:d4369386c1a958aa18a802",
   measurementId: "G-882H9VPLJT"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
